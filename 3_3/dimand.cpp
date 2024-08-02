f3be3523e01c8a4732b5aa9b0b209821ff51b3f3
Om-Trivedi9 <mr.immortalom09@gmail.com>
2024-08-02 19:25:22 +0530

    2/8/2024

---
+++ 3_3/dimand.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Company
+{
+public:
+    int comp_id;
+    char comp_name[70];
+    int comp_staff_quantity;
+    int comp_revenue;
+    int comp_import_raw_diamonds;
+    int comp_export_diamonds;
+    char comp_ceo[70];
+
+    Company(int id, char name[], int staff, int revenue, int inport_dimond, int export_dimond, char ceo)
+    {
+        cout << "enter company ID:";
+        cin >> comp_id;
+        cout << "enter company name:";
+        cin >> comp_name;
+        cout << "enter company staff quantity:";
+        cin >> comp_staff_quantity;
+        cout << "enter company revenue:";
+        cin >> comp_revenue;
+        cout << "enter company import raw diamonds:";
+        cin >> comp_import_raw_diamonds;
+        cout << "enter company export diamonds:";
+        cin >> comp_export_diamonds;
+        cout << "enter company CEO name:";
+        cin >> comp_ceo;
+        this->comp_id = id;
+        strcpy(this->comp_name, comp_name);
+        this->comp_staff_quantity = comp_staff_quantity;
+        this->comp_revenue = comp_revenue;
+        this->comp_import_raw_diamonds = comp_import_raw_diamonds;
+        this->comp_export_diamonds = comp_export_diamonds;
+        strcpy(this->comp_ceo, comp_ceo);
+    }
+    void getdata()
+    {
+        cout << "Company ID: " << comp_id << endl;
+        cout << "Company Name: " << comp_name << endl;
+        cout << "Company Staff Quantity: " << comp_staff_quantity << endl;
+        cout << "Company Revenue: " << comp_revenue << endl;
+        cout << "Company Import Raw Diamonds: " << comp_import_raw_diamonds << endl;
+        cout << "Company Export Diamonds: " << comp_export_diamonds << endl;
+        cout << "Company CEO Name: " << comp_ceo << endl;
+    }
+};
+
+main()
+{
+    int n;
+    cout << "enter number of companies: ";
+    cin >> n;
+
+    Company c[n];
+
+     
+}
---
+++ pr_2/railway.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Train
+{
+public:
+  int trainNumber;
+  char trainName[50];
+  char source[50];
+  char destination[50];
+  char trainTime[10];
+  static int tcount;
+
+public:
+  ~Train()
+  {
+    tcount--;
+  }
+
+  void settrianinput()
+  {
+    cout << "Enter train number: ";
+    cin >> this->trainNumber;
+    cout << "enter trian name:";
+    cin >> this->trainName;
+    cout << "enter source: ";
+    cin >> this->source;
+    cout << "enter destination: ";
+    cin >> this->destination;
+    cout << "enter train time: ";
+    cin >> this->trainTime;
+  }
+
+  void getdata()
+  {
+    cout << "train number: " << trainNumber << endl;
+    cout << "train name: " << trainName << endl;
+    cout << "source: " << source << endl;
+    cout << "destination: " << destination << endl;
+    cout << "trian time:" << trainTime << endl;
+  }
+};
+
+int Train::tcount = 20;
+
+class Railway
+{
+public:
+  Train t1[100];
+  int n = 0;
+
+public:
+  void traininput()
+  {
+    t1[n].settrianinput();
+
+    n++;
+  }
+
+  void display()
+  {
+    for (int i = 0; i < n; i++)
+    {
+      t1[i].getdata();
+    }
+  }
+
+  void search(int o)
+  {
+    t1[o].getdata();
+  }
+};
+
+int main()
+{
+  Railway r;
+  int choies;
+  int o;
+  do
+  {
+    cout << "enter 1 for new recods of trian" << endl;
+    cout << "enter 2 for all recods of trian" << endl;
+    cout << "enter 3 for search Train By Number of trian" << endl;
+    cout << "enter 4 for exit" << endl;
+    cout << "enter your choies";
+    cin >> choies;
+
+    switch (choies)
+    {
+
+    case 1:
+      r.traininput();
+      break;
+    case 2:
+      r.display();
+      break;
+
+    case 3:
+      cout << "enter the number of trian you want to search";
+      cin >> o;
+      r.search(o);
+      break;
+
+    case 4:
+      cout << "thank you,visit again";
+    default:
+      cout << "invalid choice";
+      break;
+    }
+
+  } while (choies != 4);
+}
